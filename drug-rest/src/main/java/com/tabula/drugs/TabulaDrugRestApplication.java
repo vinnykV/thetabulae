568838f1eb5f5d82fef50b078198501cd5f5d234
Vladyslav Vinnyk <vladyslav_vinnyk@epam.com>
2016-12-14 15:37:39 +0200

    Set Up Spring Boot

---
+++ drug-rest/src/main/java/com/tabula/drugs/TabulaDrugRestApplication.java
@@ -0,0 +1,15 @@
+package com.tabula.drugs;
+
+import com.tabula.drugs.configuration.ApplicationConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vladyslav_Vinnyk on 12/14/2016.
+ */
+@SpringBootApplication
+public class TabulaDrugRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TabulaDrugRestApplication.class, args);
+    }
+}
---
+++ drug-rest/src/main/java/com/tabula/drugs/configuration/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.tabula.drugs.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author Vladyslav_Vinnyk on 12/14/2016.
+ */
+@Profile("default")
+@Configuration
+public class ApplicationConfig {
+
+}
